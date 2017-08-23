ebb18277a595115dbcb9a9b7088513830be5eb62
akashshah92 <akashshah92@gmail.com>
2017-08-23 14:44:16 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,7 +6,7 @@ element.innerHTML='New value';
 
 var img=document.getElementById("madi");
 var marginLeft = 0;
-function moveright(){
+function moveRight(){
     marginLeft= marginLeft + 10;
     img.style.marginLeft = marginLeft + 'px';
 }
